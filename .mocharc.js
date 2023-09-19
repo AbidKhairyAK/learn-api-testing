feb78a24e98f0ed50e9f67325ecf8ec36512c574
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-09-19 16:09:04 +0700

    supertest & report portal

---
+++ .mocharc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	reporter: '@reportportal/agent-js-mocha',
+	'reporter-option': [
+		'endpoint=https://demo.reportportal.io/api/v1',
+		'apiKey=belajar_k7NByBpTQK2BVO0SG5kEjiAeIcoQiSrcjQvmOa1Vz3EarMt5Vah-G6WmV8nkQ4G0',
+		'launch=Test API Dummy JSON',
+		'project=abidkhairyak_personal',
+		'attributes=ApiTest;UnitTest',
+	],
+}
